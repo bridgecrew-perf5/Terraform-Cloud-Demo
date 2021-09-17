3d2473ab87f199403d4fd5e43ba33c60ada81670
THARUN KUMAR TALLAPALLI <43419897+findtharun@users.noreply.github.com>
2021-09-17 17:40:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "TestInstance" {
+resource "aws_instance" "instance" {
   ami           = "ami-013f17f36f8b1fefb" #use-east-1 Instance AMI
   instance_type = "t2.micro"
   tags={
