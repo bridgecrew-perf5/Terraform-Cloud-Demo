a73abc1c1a914428e147a46cfaf8d9935779b7c7
THARUN KUMAR TALLAPALLI <43419897+findtharun@users.noreply.github.com>
2021-09-17 17:39:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "TestInstance" {
   ami           = "ami-013f17f36f8b1fefb" #use-east-1 Instance AMI
   instance_type = "t2.micro"
+  tags={
+    Name ="Terraform-Cloud-Instance"
 }
